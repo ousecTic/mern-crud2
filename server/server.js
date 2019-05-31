2a08426e3ad28e797af170f55d637ba5a44a1f2a
Henry Ly <henryly213@gmail.com>
2019-05-31 12:26:05 -0500

    add post and get route

--- server/server.js
+++ server/server.js
@@ -1,2 +1,34 @@
 const express = require("express");
 const app = express();
+const PORT = process.env.PORT || 8080;
+const cors = require("cors");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+const config = require("./DB");
+
+//routes
+
+const commentRoutes = require("./routes/commentRoutes");
+
+//use
+
+app.use(cors());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use("/comments", commentRoutes);
+
+//database connections
+
+mongoose.Promise = global.Promise;
+mongoose.connect(config.DB, { useNewUrlParser: true }).then(
+  () => {
+    console.log("Database is connected");
+  },
+  err => {
+    console.log(`Database wasn't able to connect ${err}`);
+  }
+);
+
+app.listen(PORT, () => {
+  console.log(`Listening on PORT ${PORT}`);
+});
